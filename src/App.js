d93c1fe1b3db50fba0ef48a573976d6114d3addc
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2022-08-22 21:24:17 +0500

    paras

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <Parallax
-        pages={3}
+        pages={8}
         style={{
           overflow: "auto",
           top: "0",
@@ -27,52 +27,41 @@ function App() {
           backgroundImage: { sky },
         }}
       >
-        <ParallaxLayer
-          offset={0}
-          speed={0.0000001}
-          sticky={{ start: 0.05, end: 0.83 }}
-        >
+        <ParallaxLayer offset={0} speed={1} sticky={{ start: 0.28, end: 0.83 }}>
           <img id="last" src={last}></img>
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={0}
-          speed={0.0000002}
-          sticky={{ start: 0.047, end: 0.82 }}
-        >
+        <ParallaxLayer offset={0} speed={2} sticky={{ start: 0.26, end: 0.82 }}>
           <img src={middle} id="middle"></img>
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={0}
-          speed={-20}
-          sticky={{ start: 0.045, end: 0.81 }}
-        >
+        <ParallaxLayer offset={0} speed={3} sticky={{ start: 0.25, end: 0.88 }}>
           <img src={first} id="first"></img>
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
           speed={-10}
-          sticky={{ start: 0.03, end: 0.9 }}
+          factor={0.4}
+          sticky={{ start: 0.3, end: 0.9 }}
         >
           <img src={road} id="road"></img>
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
           speed={-20}
-          sticky={{ start: 0.045, end: 0.92 }}
+          sticky={{ start: 0.35, end: 0.92 }}
         >
           <img src={sign} id="sign"></img>
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
           speed={-20}
-          sticky={{ start: 0.032, end: 0.91 }}
+          sticky={{ start: 0.31, end: 0.91 }}
         >
           <img src={lampright} id="lampright"></img>
         </ParallaxLayer>
         <ParallaxLayer
           offset={0}
           speed={-20}
-          sticky={{ start: 0.05, end: 0.95 }}
+          sticky={{ start: 0.35, end: 0.95 }}
         >
           <img src={lamp2right} id="lamp2right"></img>
         </ParallaxLayer>
@@ -82,9 +71,71 @@ function App() {
         <ParallaxLayer
           offset={0}
           speed={-20}
-          sticky={{ start: 0.04, end: 0.92 }}
+          sticky={{ start: 0.37, end: 0.92 }}
         >
           <img src={lampleft} id="lampleft"></img>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.1}>
+          <div>
+            <h1> HELLO WORLD!</h1>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div>
+            <h1> HELLO WORLD2!</h1>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div>
+            <h1> HELLO WORLD3!</h1>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div style={{ height: "250px", width: "400px", color: "purple" }}>
+            <h1> HELLO WORLD4!</h1>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div>
+            <p>
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <img src={sign} style={{ height: "400px", width: "400px" }}></img>
+          </div>
         </ParallaxLayer>
       </Parallax>
     </div>
