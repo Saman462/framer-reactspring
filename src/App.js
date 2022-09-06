0ce130be79c083b924972d1cbb194f66a6ce188b
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2022-09-06 23:12:45 +0500

    new editions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import sky from "./BS.png";
 import { motion, useTransform, useScroll } from "framer-motion";
-
+import React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import lampright from "./lampr.png";
 import lamp2right from "./lamp2r.png";
@@ -15,151 +15,137 @@ import clouds from "./Clouds.png";
 function App() {
   const { scrollY } = useScroll();
   const scaleRoad = useTransform(scrollY, [0, 500], [1.3, 1.2]);
+  const scaleMountain1 = useTransform(scrollY, [0, 500], [1.14, 1]);
+  const scaleMountain2 = useTransform(scrollY, [0, 500], [1.135, 1]);
+  const scaleMountain3 = useTransform(scrollY, [0, 500], [1.13, 1]);
+
   // const scaleRoad = useTransform(scrollY, [0, 500], [1.3, 1.2]);
+  const scaleLampPost1 = useTransform(scrollY, [0, 500], [1, 1.15]);
+  const scalesign = useTransform(scrollY, [0, 500], [0.98, 1.06]);
+  const scaleLampPost2 = useTransform(scrollY, [0, 500], [0.88, 1.05]);
   const scaleme = useTransform(scrollY, [0, 500], [1, 1]);
-  // const scaleme = useTransform(scrollY, [0, 500], [1, 1]);
   return (
     <div>
+      <nav class="navbar">
+        <h1 class="logo">LIGHTS</h1>
+        <ul class="nav-links">
+          <li class="active">
+            <a href="#"></a>Home
+          </li>
+          <li>
+            <a href="#"></a>Tours
+          </li>
+          <li>
+            <a href="#"></a>Explore
+          </li>
+          <li>
+            <a href="#"></a>About
+          </li>
+          <li class="ctn">
+            <a href="#"></a>Contact
+          </li>
+        </ul>
+      </nav>
       <Parallax
-        pages={8}
+        pages={4}
         style={{
           overflow: "visible",
           top: "0",
           left: "0",
           width: "100%",
-          height: "100%",
           backgroundImage: { sky },
+          height: "100vh",
         }}
       >
-        <ParallaxLayer offset={0} speed={0} sticky={{ start: 0.2, end: 0.88 }}>
-          <img id="last" src={last}></img>
+        <ParallaxLayer offset={0.1} speed={9} sticky={{ start: 0.15, end: 2 }}>
+          <img src={clouds} id="clouds"></img>
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={0}
-          speed={-888888888}
-          sticky={{ start: 0.2, end: 0.88 }}
-        >
-          <img src={middle} id="middle"></img>
+        <ParallaxLayer offset={0} speed={0} sticky={{ start: 0.59, end: 1 }}>
+          <motion.img
+            id="last"
+            src={last}
+            style={{ ScaleY: scaleMountain3 }}
+          ></motion.img>
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={0}
-          speed={0.8}
-          sticky={{ start: 0.2, end: 0.88 }}
-        >
-          <img src={first} id="first"></img>
+        <ParallaxLayer offset={0} speed={0.4} sticky={{ start: 0.55, end: 2 }}>
+          <motion.img
+            src={middle}
+            id="middle"
+            style={{ scaleY: scaleMountain2 }}
+          ></motion.img>
         </ParallaxLayer>
-        <ParallaxLayer
-          offset={0}
-          speed={-9999999999}
-          factor={0}
-          sticky={{ start: 0.2, end: 0.88 }}
-        >
+        <ParallaxLayer offset={0} speed={0.8} sticky={{ start: 0.5, end: 1 }}>
+          <motion.img
+            src={first}
+            id="first"
+            style={{
+              scaleY: scaleMountain1,
+            }}
+          ></motion.img>
+        </ParallaxLayer>
+        <ParallaxLayer speed={0} factor={0} sticky={{ start: 0.92, end: 4 }}>
           <motion.img
             src={road}
             style={{ scaleY: scaleRoad }}
             id="road"
           ></motion.img>
         </ParallaxLayer>
-        <motion.ParallaxLayer
+        <ParallaxLayer
           offset={0}
           speed={5}
-          sticky={{ start: 0.2, end: 0.88 }}
+          sticky={{ start: 0.2, end: 2 }}
+          style={{
+            display: "flex",
+            alignItems: "flex-end",
+          }}
         >
           <motion.img
             src={sign}
             id="sign"
-            style={{ scale: scaleme }}
+            style={{
+              scaleY: scalesign,
+              marginLeft: "120px",
+              marginBottom: "260px",
+            }}
           ></motion.img>
-        </motion.ParallaxLayer>
+        </ParallaxLayer>
         {/* <ParallaxLayer offset={0} speed={0} sticky={{ start: 0.2, end: 0.88 }}>
-          <motion.img
-            src={lampright}
-            id="lampright"
-            style={{ scale: scaleme }}
-          ></motion.img> 
-        </ParallaxLayer>*/}
-        <ParallaxLayer offset={0} speed={5} sticky={{ start: 0.2, end: 0.88 }}>
+        <motion.img
+          src={lampright}
+          id="lampright"
+          style={{ scale: scaleme }}
+        ></motion.img> 
+      </ParallaxLayer>*/}
+        <ParallaxLayer
+          offset={0.5}
+          speed={95}
+          sticky={{ start: 0.38, end: 1 }}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
           <motion.img
             src={lamp2right}
-            id="lamp2right"
-            style={{ scale: scaleme }}
+            style={{
+              scaleY: scaleLampPost1,
+              height: "68%",
+              width: "8%",
+              margin: "-66px 60px",
+            }}
           ></motion.img>
         </ParallaxLayer>
-        <ParallaxLayer offset={0.1} speed={9} sticky={{ start: 0.2, end: 0.8 }}>
-          <img src={clouds} id="clouds"></img>
-        </ParallaxLayer>
+
         <ParallaxLayer
           offset={0}
           speed={3.6}
-          sticky={{ start: 0.2, end: 0.88 }}
+          sticky={{ start: 0.24, end: 0.88 }}
         >
           <motion.img
             src={lampleft}
             id="lampleft"
-            style={{ scale: scaleme }}
+            style={{ scaleY: scaleLampPost2 }}
           ></motion.img>
-        </ParallaxLayer>
-        <ParallaxLayer offset={2.1}>
-          <div>
-            <h1> HELLO WORLD!</h1>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div>
-            <h1> HELLO WORLD2!</h1>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div>
-            <h1> HELLO WORLD3!</h1>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div style={{ height: "250px", width: "400px", color: "purple" }}>
-            <h1> HELLO WORLD4!</h1>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-          <div>
-            <p>
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <img src={sign} style={{ height: "400px", width: "400px" }}></img>
-          </div>
         </ParallaxLayer>
       </Parallax>
     </div>
